stopstopw-bit/Catcha-hotel

Sell courses in the customer app: order, transfer, slip, shop confirms
Customers can now see what packages are for sale on the LIFF home page and
buy without messaging the shop. Tapping ซื้อ creates a pending order and
reveals the bank account, the exact amount, a copy button, and a slip
upload. The shop confirms payment in admin and the course is added to the
customer automatically with income recorded.

The order is deliberately not a course until the shop confirms — the money
hasn't arrived yet. confirmPackageOrder claims the row with a conditional
status update before creating anything, so a double click can't produce two
courses or two income rows.

Both money-adjacent flags carry over: confirming can be marked as carried
over from the old system (credits the customer, skips income), and the
customer always gets a LINE card when their course goes live since they
paid and are waiting — with notifyError surfaced rather than a false
"แจ้งแล้ว".

Access follows the existing shared-route pattern: /api/package-shop is in
SHARED_API, customers identify by their own lineUserId (and can only attach
a slip to their own order), while creating offers, confirming, and
cancelling require an admin session.

Verified at the API level: catalog create, customer sees offers + bank
details, order goes pending, slip attaches, confirm creates the course
(0→1) and books income (0→1), and a second confirm returns not_pending
with no duplicate course or income. Verified in the browser: admin catalog
list, customer shop card, the transfer panel with amount and account, the
"ส่งสลิปแล้ว" state, and the admin pending-order row with the slip.

Not click-verified: the confirm button's own path, because the automation
auto-dismisses its native confirm() dialog — the endpoint behind it is the
one covered by the API test above.

Co-Authored-By: Claude Opus 4.8 <noreply@anthropic.com>

webapp/src/components/PackageShopSection.tsx
@@ -1,0 +1,255 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import { useLiff } from "@/components/LiffProvider";
+import { toJpegDataUrl } from "@/lib/image-convert";
+
+type Offer = {
+  id: string;
+  name: string;
+  totalUses: number;
+  price: number;
+  description?: string;
+};
+
+type Order = {
+  id: string;
+  name: string;
+  totalUses: number;
+  price: number;
+  status: "pending" | "paid" | "cancelled";
+  slipUrl?: string;
+  createdAt: string;
+};
+
+type Payment = { bankName: string; accountNumber: string; accountName: string };
+
+function fileToDataUrl(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(String(reader.result));
+    reader.onerror = () => reject(new Error("อ่านไฟล์ไม่สำเร็จ"));
+    reader.readAsDataURL(file);
+  });
+}
+
+/**
+ * ร้านขายคอร์สในแอปลูกค้า — เห็นว่ามีอะไรขาย กดซื้อ โอน แล้วแนบสลิปได้ในที่เดียว
+ * คอร์สจะเข้าระบบก็ต่อเมื่อร้านกดยืนยันรับเงินแล้วเท่านั้น
+ *
+ * ไม่มีของขายและไม่มีออร์เดอร์ค้าง = ซ่อนทั้งช่อง
+ */
+export function PackageShopSection() {
+  const { profile } = useLiff();
+  const [offers, setOffers] = useState<Offer[]>([]);
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [payment, setPayment] = useState<Payment | null>(null);
+  const [buying, setBuying] = useState("");
+  const [uploading, setUploading] = useState("");
+  const [copied, setCopied] = useState(false);
+  const [msg, setMsg] = useState("");
+
+  const load = useCallback(async () => {
+    if (!profile?.lineUserId) return;
+    try {
+      const r = await fetch(
+        `/api/package-shop?lineUserId=${encodeURIComponent(profile.lineUserId)}`
+      );
+      const d = await r.json();
+      setOffers(d.offers || []);
+      setOrders(d.orders || []);
+      setPayment(d.payment || null);
+    } catch {
+      /* โหลดไม่ได้ = ซ่อนช่องไปเลย ดีกว่าโชว์ช่องว่าง */
+    }
+  }, [profile?.lineUserId]);
+
+  useEffect(() => {
+    load();
+  }, [load]);
+
+  const pending = orders.filter((o) => o.status === "pending");
+
+  const buy = async (offer: Offer) => {
+    if (!profile?.lineUserId) return;
+    if (
+      !confirm(
+        `สั่งซื้อ "${offer.name}" (${offer.totalUses} ครั้ง)\n` +
+          `ยอดที่ต้องโอน ${offer.price.toLocaleString()} บาท\n\n` +
+          `กดตกลงแล้วจะมีเลขบัญชีให้โอน แล้วแนบสลิปได้เลยนะคะ`
+      )
+    )
+      return;
+    setBuying(offer.id);
+    setMsg("");
+    try {
+      const res = await fetch("/api/package-shop", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          action: "order",
+          lineUserId: profile.lineUserId,
+          offerId: offer.id,
+        }),
+      });
+      if (res.ok) {
+        setMsg("สั่งซื้อแล้ว — โอนตามเลขบัญชีด้านล่าง แล้วแนบสลิปได้เลยค่ะ 🧡");
+        load();
+      } else {
+        const d = await res.json().catch(() => ({}));
+        setMsg(
+          d.error === "not_registered"
+            ? "กรุณาสมัครสมาชิกก่อนสั่งซื้อนะคะ"
+            : "สั่งซื้อไม่สำเร็จ ลองใหม่อีกครั้งนะคะ"
+        );
+      }
+    } finally {
+      setBuying("");
+    }
+  };
+
+  const uploadSlip = async (orderId: string, file: File) => {
+    if (!profile?.lineUserId) return;
+    setUploading(orderId);
+    setMsg("");
+    try {
+      // ย่อรูปก่อนส่ง — สลิปจากมือถือไฟล์ใหญ่มาก ถ้าย่อไม่ได้ค่อยส่งไฟล์เดิม
+      const slip = await toJpegDataUrl(file, 1200).catch(() => fileToDataUrl(file));
+      const res = await fetch("/api/package-shop", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          action: "slip",
+          lineUserId: profile.lineUserId,
+          orderId,
+          slip,
+        }),
+      });
+      setMsg(
+        res.ok
+          ? "ส่งสลิปแล้ว รอร้านตรวจสอบสักครู่นะคะ 🧾"
+          : "ส่งสลิปไม่สำเร็จ ลองใหม่อีกครั้งนะคะ"
+      );
+      if (res.ok) load();
+    } finally {
+      setUploading("");
+    }
+  };
+
+  const copyAccount = async () => {
+    if (!payment?.accountNumber) return;
+    try {
+      await navigator.clipboard.writeText(payment.accountNumber);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    } catch {
+      /* คลิปบอร์ดใช้ไม่ได้บนบางเครื่อง — ลูกค้าพิมพ์เองได้ */
+    }
+  };
+
+  if (offers.length === 0 && pending.length === 0) return null;
+
+  return (
+    <section className="mb-4 rounded-catcha border-2 border-honey/50 bg-gradient-to-br from-honey/20 via-card to-latte/10 p-4 shadow-catcha">
+      <p className="mb-1 text-sm font-extrabold text-catcha-chocolate">
+        🛍️ คอร์สที่เปิดขาย
+      </p>
+      <p className="mb-3 text-[10px] text-brown-faint">
+        ซื้อล่วงหน้าคุ้มกว่า · ใช้ได้ทุกครั้งที่มาใช้บริการ
+      </p>
+
+      {offers.length > 0 && (
+        <div className="mb-3 space-y-2">
+          {offers.map((o) => (
+            <div key={o.id} className="rounded-catcha-sm bg-card/85 px-3 py-2.5">
+              <div className="flex items-start justify-between gap-2">
+                <div className="min-w-0">
+                  <p className="text-xs font-bold text-brown">{o.name}</p>
+                  <p className="text-[10px] text-brown-soft">
+                    ใช้ได้ {o.totalUses} ครั้ง
+                    {o.description ? ` · ${o.description}` : ""}
+                  </p>
+                </div>
+                <div className="shrink-0 text-right">
+                  <p className="text-sm font-extrabold text-latte-deep">
+                    {o.price.toLocaleString()}฿
+                  </p>
+                  <button
+                    type="button"
+                    disabled={buying === o.id}
+                    onClick={() => buy(o)}
+                    className="mt-1 rounded-full bg-latte-deep px-3 py-1 text-[10px] font-extrabold text-card disabled:opacity-40"
+                  >
+                    {buying === o.id ? "…" : "ซื้อเลย"}
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {/* รอโอน — โชว์เลขบัญชี ยอดที่ต้องโอน และปุ่มแนบสลิป */}
+      {pending.map((o) => (
+        <div
+          key={o.id}
+          className="mb-2 rounded-catcha-sm border border-wait/40 bg-paper/70 px-3 py-2.5"
+        >
+          <p className="text-xs font-extrabold text-wait">⏳ รอโอนเงิน · {o.name}</p>
+          <p className="mt-0.5 text-[10px] text-brown-soft">
+            ยอดที่ต้องโอน{" "}
+            <span className="text-sm font-extrabold text-catcha-chocolate">
+              {o.price.toLocaleString()} บาท
+            </span>
+          </p>
+
+          {payment?.accountNumber && (
+            <div className="mt-2 rounded-catcha-sm bg-card px-2.5 py-2">
+              <p className="text-[10px] font-bold text-brown-soft">{payment.bankName}</p>
+              <p className="text-sm font-extrabold text-ok">{payment.accountNumber}</p>
+              {payment.accountName && (
+                <p className="text-[10px] text-brown-faint">
+                  ชื่อบัญชี: {payment.accountName}
+                </p>
+              )}
+              <button
+                type="button"
+                onClick={copyAccount}
+                className="mt-1.5 w-full rounded-catcha-sm bg-latte/25 py-1.5 text-[10px] font-extrabold text-catcha-chocolate"
+              >
+                {copied ? "คัดลอกแล้ว ✓" : "📋 คัดลอกเลขบัญชี"}
+              </button>
+            </div>
+          )}
+
+          {o.slipUrl ? (
+            <p className="mt-2 rounded-catcha-sm bg-sage/15 px-2.5 py-1.5 text-[10px] font-bold text-ok">
+              ✅ ส่งสลิปแล้ว — รอร้านยืนยันรับเงิน คอร์สจะเข้าให้อัตโนมัติค่ะ
+            </p>
+          ) : (
+            <label className="mt-2 block cursor-pointer rounded-catcha-sm bg-honey/40 py-2 text-center text-[11px] font-extrabold text-catcha-chocolate">
+              {uploading === o.id ? "กำลังส่ง…" : "📎 แนบสลิปโอนเงิน"}
+              <input
+                type="file"
+                accept="image/*"
+                className="hidden"
+                disabled={uploading === o.id}
+                onChange={(e) => {
+                  const f = e.target.files?.[0];
+                  if (f) uploadSlip(o.id, f);
+                  e.target.value = "";
+                }}
+              />
+            </label>
+          )}
+        </div>
+      ))}
+
+      {msg && (
+        <p className="mt-1 rounded-catcha-sm bg-card px-2.5 py-1.5 text-[10px] font-bold text-brown-soft">
+          {msg}
+        </p>
+      )}
+    </section>
+  );
+}

petflowth/pet-flow

src/components/PackageShopSection.tsx
@@ -1,0 +1,255 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import { useLiff } from "@/components/LiffProvider";
+import { toJpegDataUrl } from "@/lib/image-convert";
+
+type Offer = {
+  id: string;
+  name: string;
+  totalUses: number;
+  price: number;
+  description?: string;
+};
+
+type Order = {
+  id: string;
+  name: string;
+  totalUses: number;
+  price: number;
+  status: "pending" | "paid" | "cancelled";
+  slipUrl?: string;
+  createdAt: string;
+};
+
+type Payment = { bankName: string; accountNumber: string; accountName: string };
+
+function fileToDataUrl(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(String(reader.result));
+    reader.onerror = () => reject(new Error("อ่านไฟล์ไม่สำเร็จ"));
+    reader.readAsDataURL(file);
+  });
+}
+
+/**
+ * ร้านขายคอร์สในแอปลูกค้า — เห็นว่ามีอะไรขาย กดซื้อ โอน แล้วแนบสลิปได้ในที่เดียว
+ * คอร์สจะเข้าระบบก็ต่อเมื่อร้านกดยืนยันรับเงินแล้วเท่านั้น
+ *
+ * ไม่มีของขายและไม่มีออร์เดอร์ค้าง = ซ่อนทั้งช่อง
+ */
+export function PackageShopSection() {
+  const { profile } = useLiff();
+  const [offers, setOffers] = useState<Offer[]>([]);
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [payment, setPayment] = useState<Payment | null>(null);
+  const [buying, setBuying] = useState("");
+  const [uploading, setUploading] = useState("");
+  const [copied, setCopied] = useState(false);
+  const [msg, setMsg] = useState("");
+
+  const load = useCallback(async () => {
+    if (!profile?.lineUserId) return;
+    try {
+      const r = await fetch(
+        `/api/package-shop?lineUserId=${encodeURIComponent(profile.lineUserId)}`
+      );
+      const d = await r.json();
+      setOffers(d.offers || []);
+      setOrders(d.orders || []);
+      setPayment(d.payment || null);
+    } catch {
+      /* โหลดไม่ได้ = ซ่อนช่องไปเลย ดีกว่าโชว์ช่องว่าง */
+    }
+  }, [profile?.lineUserId]);
+
+  useEffect(() => {
+    load();
+  }, [load]);
+
+  const pending = orders.filter((o) => o.status === "pending");
+
+  const buy = async (offer: Offer) => {
+    if (!profile?.lineUserId) return;
+    if (
+      !confirm(
+        `สั่งซื้อ "${offer.name}" (${offer.totalUses} ครั้ง)\n` +
+          `ยอดที่ต้องโอน ${offer.price.toLocaleString()} บาท\n\n` +
+          `กดตกลงแล้วจะมีเลขบัญชีให้โอน แล้วแนบสลิปได้เลยนะคะ`
+      )
+    )
+      return;
+    setBuying(offer.id);
+    setMsg("");
+    try {
+      const res = await fetch("/api/package-shop", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          action: "order",
+          lineUserId: profile.lineUserId,
+          offerId: offer.id,
+        }),
+      });
+      if (res.ok) {
+        setMsg("สั่งซื้อแล้ว — โอนตามเลขบัญชีด้านล่าง แล้วแนบสลิปได้เลยค่ะ 🧡");
+        load();
+      } else {
+        const d = await res.json().catch(() => ({}));
+        setMsg(
+          d.error === "not_registered"
+            ? "กรุณาสมัครสมาชิกก่อนสั่งซื้อนะคะ"
+            : "สั่งซื้อไม่สำเร็จ ลองใหม่อีกครั้งนะคะ"
+        );
+      }
+    } finally {
+      setBuying("");
+    }
+  };
+
+  const uploadSlip = async (orderId: string, file: File) => {
+    if (!profile?.lineUserId) return;
+    setUploading(orderId);
+    setMsg("");
+    try {
+      // ย่อรูปก่อนส่ง — สลิปจากมือถือไฟล์ใหญ่มาก ถ้าย่อไม่ได้ค่อยส่งไฟล์เดิม
+      const slip = await toJpegDataUrl(file, 1200).catch(() => fileToDataUrl(file));
+      const res = await fetch("/api/package-shop", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          action: "slip",
+          lineUserId: profile.lineUserId,
+          orderId,
+          slip,
+        }),
+      });
+      setMsg(
+        res.ok
+          ? "ส่งสลิปแล้ว รอร้านตรวจสอบสักครู่นะคะ 🧾"
+          : "ส่งสลิปไม่สำเร็จ ลองใหม่อีกครั้งนะคะ"
+      );
+      if (res.ok) load();
+    } finally {
+      setUploading("");
+    }
+  };
+
+  const copyAccount = async () => {
+    if (!payment?.accountNumber) return;
+    try {
+      await navigator.clipboard.writeText(payment.accountNumber);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    } catch {
+      /* คลิปบอร์ดใช้ไม่ได้บนบางเครื่อง — ลูกค้าพิมพ์เองได้ */
+    }
+  };
+
+  if (offers.length === 0 && pending.length === 0) return null;
+
+  return (
+    <section className="mb-4 rounded-petflow border-2 border-honey/50 bg-gradient-to-br from-honey/20 via-card to-latte/10 p-4 shadow-petflow">
+      <p className="mb-1 text-sm font-extrabold text-petflow-chocolate">
+        🛍️ คอร์สที่เปิดขาย
+      </p>
+      <p className="mb-3 text-[10px] text-brown-faint">
+        ซื้อล่วงหน้าคุ้มกว่า · ใช้ได้ทุกครั้งที่มาใช้บริการ
+      </p>
+
+      {offers.length > 0 && (
+        <div className="mb-3 space-y-2">
+          {offers.map((o) => (
+            <div key={o.id} className="rounded-petflow-sm bg-card/85 px-3 py-2.5">
+              <div className="flex items-start justify-between gap-2">
+                <div className="min-w-0">
+                  <p className="text-xs font-bold text-brown">{o.name}</p>
+                  <p className="text-[10px] text-brown-soft">
+                    ใช้ได้ {o.totalUses} ครั้ง
+                    {o.description ? ` · ${o.description}` : ""}
+                  </p>
+                </div>
+                <div className="shrink-0 text-right">
+                  <p className="text-sm font-extrabold text-latte-deep">
+                    {o.price.toLocaleString()}฿
+                  </p>
+                  <button
+                    type="button"
+                    disabled={buying === o.id}
+                    onClick={() => buy(o)}
+                    className="mt-1 rounded-full bg-latte-deep px-3 py-1 text-[10px] font-extrabold text-card disabled:opacity-40"
+                  >
+                    {buying === o.id ? "…" : "ซื้อเลย"}
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {/* รอโอน — โชว์เลขบัญชี ยอดที่ต้องโอน และปุ่มแนบสลิป */}
+      {pending.map((o) => (
+        <div
+          key={o.id}
+          className="mb-2 rounded-petflow-sm border border-wait/40 bg-paper/70 px-3 py-2.5"
+        >
+          <p className="text-xs font-extrabold text-wait">⏳ รอโอนเงิน · {o.name}</p>
+          <p className="mt-0.5 text-[10px] text-brown-soft">
+            ยอดที่ต้องโอน{" "}
+            <span className="text-sm font-extrabold text-petflow-chocolate">
+              {o.price.toLocaleString()} บาท
+            </span>
+          </p>
+
+          {payment?.accountNumber && (
+            <div className="mt-2 rounded-petflow-sm bg-card px-2.5 py-2">
+              <p className="text-[10px] font-bold text-brown-soft">{payment.bankName}</p>
+              <p className="text-sm font-extrabold text-ok">{payment.accountNumber}</p>
+              {payment.accountName && (
+                <p className="text-[10px] text-brown-faint">
+                  ชื่อบัญชี: {payment.accountName}
+                </p>
+              )}
+              <button
+                type="button"
+                onClick={copyAccount}
+                className="mt-1.5 w-full rounded-petflow-sm bg-latte/25 py-1.5 text-[10px] font-extrabold text-petflow-chocolate"
+              >
+                {copied ? "คัดลอกแล้ว ✓" : "📋 คัดลอกเลขบัญชี"}
+              </button>
+            </div>
+          )}
+
+          {o.slipUrl ? (
+            <p className="mt-2 rounded-petflow-sm bg-sage/15 px-2.5 py-1.5 text-[10px] font-bold text-ok">
+              ✅ ส่งสลิปแล้ว — รอร้านยืนยันรับเงิน คอร์สจะเข้าให้อัตโนมัติค่ะ
+            </p>
+          ) : (
+            <label className="mt-2 block cursor-pointer rounded-petflow-sm bg-honey/40 py-2 text-center text-[11px] font-extrabold text-petflow-chocolate">
+              {uploading === o.id ? "กำลังส่ง…" : "📎 แนบสลิปโอนเงิน"}
+              <input
+                type="file"
+                accept="image/*"
+                className="hidden"
+                disabled={uploading === o.id}
+                onChange={(e) => {
+                  const f = e.target.files?.[0];
+                  if (f) uploadSlip(o.id, f);
+                  e.target.value = "";
+                }}
+              />
+            </label>
+          )}
+        </div>
+      ))}
+
+      {msg && (
+        <p className="mt-1 rounded-petflow-sm bg-card px-2.5 py-1.5 text-[10px] font-bold text-brown-soft">
+          {msg}
+        </p>
+      )}
+    </section>
+  );
+}
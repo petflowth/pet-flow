stopstopw-bit/Catcha-hotel

Give staff accounts their own page and courses a cover image
Staff management was buried at the bottom of the settings page, next to JSON
import and database migrations — nothing to do with each other. It gets its
own menu entry now.

Courses sold in the app can carry a photo, and the customer-facing cards were
rebuilt around it: two-up grid, per-session price worked out for them, price
and buy button on one line. Courses without a photo still look right.

Also stops duplicate orders: tapping the buy button three times used to make
three identical pending orders, which the shop could confirm three times over
for one transfer. Repeat taps now land on the order already waiting.

Co-Authored-By: Claude Opus 4.8 <noreply@anthropic.com>

webapp/src/components/PackageShopSection.tsx
@@ -10,6 +10,7 @@ type Offer = {
   totalUses: number;
   price: number;
   description?: string;
+  imageUrl?: string;
 };
 
 type Order = {
@@ -159,33 +160,60 @@ export function PackageShopSection() {
       </p>
 
       {offers.length > 0 && (
-        <div className="mb-3 space-y-2">
-          {offers.map((o) => (
-            <div key={o.id} className="rounded-catcha-sm bg-card/85 px-3 py-2.5">
-              <div className="flex items-start justify-between gap-2">
-                <div className="min-w-0">
-                  <p className="text-xs font-bold text-brown">{o.name}</p>
-                  <p className="text-[10px] text-brown-soft">
-                    ใช้ได้ {o.totalUses} ครั้ง
-                    {o.description ? ` · ${o.description}` : ""}
+        <div className="mb-3 grid gap-2.5 sm:grid-cols-2">
+          {offers.map((o) => {
+            const perUse = o.totalUses > 0 ? Math.round(o.price / o.totalUses) : 0;
+            return (
+              <div
+                key={o.id}
+                className="overflow-hidden rounded-catcha-sm bg-card/90 shadow-catcha-sm"
+              >
+                {o.imageUrl && (
+                  // eslint-disable-next-line @next/next/no-img-element
+                  <img
+                    src={o.imageUrl}
+                    alt={o.name}
+                    loading="lazy"
+                    className="h-28 w-full object-cover"
+                  />
+                )}
+                <div className="p-3">
+                  <p className="text-sm font-extrabold leading-snug text-catcha-chocolate">
+                    {o.name}
                   </p>
-                </div>
-                <div className="shrink-0 text-right">
-                  <p className="text-sm font-extrabold text-latte-deep">
-                    {o.price.toLocaleString()}฿
+                  <p className="mt-0.5 flex flex-wrap items-center gap-x-1.5 text-[10px] font-bold text-brown-soft">
+                    <span className="rounded-full bg-sage/20 px-2 py-0.5 text-ok">
+                      ใช้ได้ {o.totalUses} ครั้ง
+                    </span>
+                    {perUse > 0 && (
+                      <span className="text-brown-faint">
+                        ตกครั้งละ {perUse.toLocaleString()}฿
+                      </span>
+                    )}
                   </p>
-                  <button
-                    type="button"
-                    disabled={buying === o.id}
-                    onClick={() => buy(o)}
-                    className="mt-1 rounded-full bg-latte-deep px-3 py-1 text-[10px] font-extrabold text-card disabled:opacity-40"
-                  >
-                    {buying === o.id ? "…" : "ซื้อเลย"}
-                  </button>
+                  {o.description && (
+                    <p className="mt-1 text-[10px] leading-relaxed text-brown-faint">
+                      {o.description}
+                    </p>
+                  )}
+                  <div className="mt-2.5 flex items-end justify-between gap-2">
+                    <p className="text-lg font-extrabold leading-none text-latte-deep">
+                      {o.price.toLocaleString()}
+                      <span className="text-xs">฿</span>
+                    </p>
+                    <button
+                      type="button"
+                      disabled={buying === o.id}
+                      onClick={() => buy(o)}
+                      className="rounded-full bg-latte-deep px-4 py-1.5 text-[11px] font-extrabold text-card shadow-catcha-sm disabled:opacity-40"
+                    >
+                      {buying === o.id ? "…" : "ซื้อเลย"}
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       )}
 

webapp/src/components/StaffSection.tsx
@@ -1,0 +1,231 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import { ADMIN_TABS } from "@/components/AdminNav";
+
+export type StaffItem = {
+  id: string;
+  name: string;
+  code: string;
+  menus: string[];
+  active: boolean;
+};
+
+/**
+ * จัดการบัญชีพนักงาน — เจ้าของเท่านั้นที่เห็น (API ตอบ 403 ให้พนักงาน)
+ *
+ * แยกออกมาจากหน้าตั้งค่าเพราะมันเป็นงานคนละเรื่องกับการตั้งค่าระบบ
+ * และเจ้าของร้านหาไม่เจอเวลามันซ่อนอยู่ท้ายหน้า
+ */
+export function StaffSection() {
+  const [staff, setStaff] = useState<StaffItem[]>([]);
+  const [isOwner, setIsOwner] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const [msg, setMsg] = useState("");
+  const [newName, setNewName] = useState("");
+  const [newCode, setNewCode] = useState("");
+  const [newMenus, setNewMenus] = useState<string[]>([
+    "/admin",
+    "/admin/schedule",
+    "/admin/bookings/new",
+    "/admin/customers",
+    "/admin/billing",
+  ]);
+  const [busy, setBusy] = useState(false);
+
+  const adminCode =
+    typeof window !== "undefined" ? sessionStorage.getItem("catcha-admin") || "" : "";
+
+  const load = useCallback(async () => {
+    try {
+      const res = await fetch("/api/admin/staff", {
+        headers: { "x-admin-code": adminCode },
+      });
+      if (res.status === 403) {
+        setIsOwner(false);
+        setLoaded(true);
+        return;
+      }
+      const d = await res.json();
+      setStaff(d.staff || []);
+      setIsOwner(true);
+    } catch {
+      /* เงียบ — โชว์เป็นลิสต์ว่าง */
+    }
+    setLoaded(true);
+  }, [adminCode]);
+
+  useEffect(() => {
+    load();
+  }, [load]);
+
+  const toggleMenu = (list: string[], href: string) =>
+    list.includes(href) ? list.filter((h) => h !== href) : [...list, href];
+
+  const addStaff = async () => {
+    setBusy(true);
+    setMsg("");
+    try {
+      const res = await fetch("/api/admin/staff", {
+        method: "POST",
+        headers: { "Content-Type": "application/json", "x-admin-code": adminCode },
+        body: JSON.stringify({ action: "add", name: newName, code: newCode, menus: newMenus }),
+      });
+      const d = await res.json().catch(() => ({}));
+      if (res.ok) {
+        setMsg(`✅ เพิ่ม "${newName}" แล้ว — ให้พนักงานใช้รหัสนี้ล็อกอินได้เลย`);
+        setNewName("");
+        setNewCode("");
+        load();
+      } else {
+        setMsg(`❌ ${d.error || "เพิ่มไม่สำเร็จ"}`);
+      }
+    } finally {
+      setBusy(false);
+    }
+  };
+
+  const patchStaff = async (id: string, patch: Record<string, unknown>) => {
+    await fetch("/api/admin/staff", {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json", "x-admin-code": adminCode },
+      body: JSON.stringify({ id, ...patch }),
+    });
+    load();
+  };
+
+  const removeStaff = async (s: StaffItem) => {
+    if (!confirm(`ลบบัญชีพนักงาน "${s.name}"?`)) return;
+    await fetch(`/api/admin/staff?id=${s.id}`, {
+      method: "DELETE",
+      headers: { "x-admin-code": adminCode },
+    });
+    load();
+  };
+
+  if (!loaded) {
+    return <p className="py-10 text-center text-sm text-brown-soft">กำลังโหลด…</p>;
+  }
+  if (!isOwner) {
+    return (
+      <p className="rounded-catcha bg-card p-4 text-xs font-bold text-brown-soft shadow-catcha-sm">
+        🔒 หน้านี้สำหรับเจ้าของร้านเท่านั้นค่ะ
+      </p>
+    );
+  }
+
+  const menuPicker = (selected: string[], onToggle: (href: string) => void) => (
+    <div className="grid grid-cols-2 gap-1.5">
+      {ADMIN_TABS.map((t) => (
+        <label key={t.href} className="flex items-center gap-1.5 text-[11px] text-brown">
+          <input
+            type="checkbox"
+            checked={selected.includes(t.href)}
+            onChange={() => onToggle(t.href)}
+            className="h-3.5 w-3.5 accent-[#4A7348]"
+          />
+          {t.icon} {t.label}
+        </label>
+      ))}
+    </div>
+  );
+
+  return (
+    <div className="space-y-4">
+      <section className="rounded-catcha bg-card p-4 shadow-catcha-sm">
+        <h2 className="text-sm font-extrabold text-catcha-chocolate">
+          👥 บัญชีพนักงาน ({staff.length})
+        </h2>
+        <p className="mb-3 text-[10px] text-brown-soft">
+          สร้างรหัสให้พนักงานแต่ละคน แล้วติ๊กเลือกว่าให้เห็นเมนูไหนบ้าง —
+          พนักงานล็อกอินด้วยรหัสของตัวเองที่หน้าเดิม จะเห็นเฉพาะเมนูที่เลือกไว้
+        </p>
+
+        {staff.length === 0 && (
+          <p className="mb-2 rounded-catcha-sm bg-paper/60 px-3 py-2.5 text-xs text-brown-soft">
+            ยังไม่มีพนักงาน — เพิ่มคนแรกได้ที่ช่องด้านล่างเลยค่ะ
+          </p>
+        )}
+
+        {staff.map((s) => (
+          <details
+            key={s.id}
+            className="mb-2 rounded-catcha-sm border border-catcha-line bg-paper/50 p-2.5"
+          >
+            <summary className="flex cursor-pointer list-none items-center justify-between gap-2">
+              <span className="min-w-0 truncate text-xs font-bold text-brown">
+                {s.active ? "🟢" : "⏸️"} {s.name}
+                <span className="ml-2 font-normal text-brown-faint">
+                  รหัส: {s.code} · เห็น {s.menus.length} เมนู
+                </span>
+              </span>
+              <span className="shrink-0 text-[10px] font-bold text-latte-deep">แก้ไข ▾</span>
+            </summary>
+            <div className="mt-2 space-y-2 border-t border-catcha-line pt-2">
+              {menuPicker(s.menus, (href) =>
+                patchStaff(s.id, { menus: toggleMenu(s.menus, href) })
+              )}
+              <div className="flex flex-wrap gap-2">
+                <button
+                  type="button"
+                  onClick={() => patchStaff(s.id, { active: !s.active })}
+                  className="rounded-full bg-card px-3 py-1 text-[10px] font-bold text-brown-soft"
+                >
+                  {s.active ? "⏸️ พักการใช้งาน" : "▶️ เปิดใช้งาน"}
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    const code = prompt("ตั้งรหัสใหม่ (อย่างน้อย 4 ตัว):", s.code);
+                    if (code && code.trim().length >= 4) patchStaff(s.id, { code: code.trim() });
+                  }}
+                  className="rounded-full bg-card px-3 py-1 text-[10px] font-bold text-brown-soft"
+                >
+                  🔑 เปลี่ยนรหัส
+                </button>
+                <button
+                  type="button"
+                  onClick={() => removeStaff(s)}
+                  className="rounded-full bg-wait/10 px-3 py-1 text-[10px] font-bold text-wait"
+                >
+                  🗑️ ลบ
+                </button>
+              </div>
+            </div>
+          </details>
+        ))}
+      </section>
+
+      <section className="rounded-catcha bg-card p-4 shadow-catcha-sm">
+        <h2 className="mb-2 text-sm font-extrabold text-catcha-chocolate">
+          ➕ เพิ่มพนักงานใหม่
+        </h2>
+        <div className="mb-2 grid gap-2 sm:grid-cols-2">
+          <input
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+            placeholder="ชื่อพนักงาน"
+            className="rounded-catcha-sm border border-catcha-line bg-paper px-2.5 py-2 text-xs"
+          />
+          <input
+            value={newCode}
+            onChange={(e) => setNewCode(e.target.value)}
+            placeholder="รหัสเข้าใช้ (4 ตัวขึ้นไป)"
+            className="rounded-catcha-sm border border-catcha-line bg-paper px-2.5 py-2 text-xs"
+          />
+        </div>
+        <p className="mb-1 text-[10px] font-bold text-brown-soft">เมนูที่ให้เห็น:</p>
+        {menuPicker(newMenus, (href) => setNewMenus((prev) => toggleMenu(prev, href)))}
+        <button
+          type="button"
+          disabled={busy || !newName.trim() || newCode.trim().length < 4}
+          onClick={addStaff}
+          className="mt-2 w-full rounded-catcha-sm bg-latte-deep py-2.5 text-xs font-extrabold text-card disabled:opacity-40"
+        >
+          {busy ? "กำลังเพิ่ม…" : "➕ เพิ่มพนักงาน"}
+        </button>
+        {msg && <p className="mt-2 text-[11px] font-bold text-brown">{msg}</p>}
+      </section>
+    </div>
+  );
+}

petflowth/pet-flow

src/components/PackageShopSection.tsx
@@ -10,6 +10,7 @@ type Offer = {
   totalUses: number;
   price: number;
   description?: string;
+  imageUrl?: string;
 };
 
 type Order = {
@@ -159,33 +160,60 @@ export function PackageShopSection() {
       </p>
 
       {offers.length > 0 && (
-        <div className="mb-3 space-y-2">
-          {offers.map((o) => (
-            <div key={o.id} className="rounded-petflow-sm bg-card/85 px-3 py-2.5">
-              <div className="flex items-start justify-between gap-2">
-                <div className="min-w-0">
-                  <p className="text-xs font-bold text-brown">{o.name}</p>
-                  <p className="text-[10px] text-brown-soft">
-                    ใช้ได้ {o.totalUses} ครั้ง
-                    {o.description ? ` · ${o.description}` : ""}
+        <div className="mb-3 grid gap-2.5 sm:grid-cols-2">
+          {offers.map((o) => {
+            const perUse = o.totalUses > 0 ? Math.round(o.price / o.totalUses) : 0;
+            return (
+              <div
+                key={o.id}
+                className="overflow-hidden rounded-petflow-sm bg-card/90 shadow-petflow-sm"
+              >
+                {o.imageUrl && (
+                  // eslint-disable-next-line @next/next/no-img-element
+                  <img
+                    src={o.imageUrl}
+                    alt={o.name}
+                    loading="lazy"
+                    className="h-28 w-full object-cover"
+                  />
+                )}
+                <div className="p-3">
+                  <p className="text-sm font-extrabold leading-snug text-petflow-chocolate">
+                    {o.name}
                   </p>
-                </div>
-                <div className="shrink-0 text-right">
-                  <p className="text-sm font-extrabold text-latte-deep">
-                    {o.price.toLocaleString()}฿
+                  <p className="mt-0.5 flex flex-wrap items-center gap-x-1.5 text-[10px] font-bold text-brown-soft">
+                    <span className="rounded-full bg-sage/20 px-2 py-0.5 text-ok">
+                      ใช้ได้ {o.totalUses} ครั้ง
+                    </span>
+                    {perUse > 0 && (
+                      <span className="text-brown-faint">
+                        ตกครั้งละ {perUse.toLocaleString()}฿
+                      </span>
+                    )}
                   </p>
-                  <button
-                    type="button"
-                    disabled={buying === o.id}
-                    onClick={() => buy(o)}
-                    className="mt-1 rounded-full bg-latte-deep px-3 py-1 text-[10px] font-extrabold text-card disabled:opacity-40"
-                  >
-                    {buying === o.id ? "…" : "ซื้อเลย"}
-                  </button>
+                  {o.description && (
+                    <p className="mt-1 text-[10px] leading-relaxed text-brown-faint">
+                      {o.description}
+                    </p>
+                  )}
+                  <div className="mt-2.5 flex items-end justify-between gap-2">
+                    <p className="text-lg font-extrabold leading-none text-latte-deep">
+                      {o.price.toLocaleString()}
+                      <span className="text-xs">฿</span>
+                    </p>
+                    <button
+                      type="button"
+                      disabled={buying === o.id}
+                      onClick={() => buy(o)}
+                      className="rounded-full bg-latte-deep px-4 py-1.5 text-[11px] font-extrabold text-card shadow-petflow-sm disabled:opacity-40"
+                    >
+                      {buying === o.id ? "…" : "ซื้อเลย"}
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       )}
 

src/components/StaffSection.tsx
@@ -1,0 +1,231 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import { ADMIN_TABS } from "@/components/AdminNav";
+
+export type StaffItem = {
+  id: string;
+  name: string;
+  code: string;
+  menus: string[];
+  active: boolean;
+};
+
+/**
+ * จัดการบัญชีพนักงาน — เจ้าของเท่านั้นที่เห็น (API ตอบ 403 ให้พนักงาน)
+ *
+ * แยกออกมาจากหน้าตั้งค่าเพราะมันเป็นงานคนละเรื่องกับการตั้งค่าระบบ
+ * และเจ้าของร้านหาไม่เจอเวลามันซ่อนอยู่ท้ายหน้า
+ */
+export function StaffSection() {
+  const [staff, setStaff] = useState<StaffItem[]>([]);
+  const [isOwner, setIsOwner] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const [msg, setMsg] = useState("");
+  const [newName, setNewName] = useState("");
+  const [newCode, setNewCode] = useState("");
+  const [newMenus, setNewMenus] = useState<string[]>([
+    "/admin",
+    "/admin/schedule",
+    "/admin/bookings/new",
+    "/admin/customers",
+    "/admin/billing",
+  ]);
+  const [busy, setBusy] = useState(false);
+
+  const adminCode =
+    typeof window !== "undefined" ? sessionStorage.getItem("petflow-admin") || "" : "";
+
+  const load = useCallback(async () => {
+    try {
+      const res = await fetch("/api/admin/staff", {
+        headers: { "x-admin-code": adminCode },
+      });
+      if (res.status === 403) {
+        setIsOwner(false);
+        setLoaded(true);
+        return;
+      }
+      const d = await res.json();
+      setStaff(d.staff || []);
+      setIsOwner(true);
+    } catch {
+      /* เงียบ — โชว์เป็นลิสต์ว่าง */
+    }
+    setLoaded(true);
+  }, [adminCode]);
+
+  useEffect(() => {
+    load();
+  }, [load]);
+
+  const toggleMenu = (list: string[], href: string) =>
+    list.includes(href) ? list.filter((h) => h !== href) : [...list, href];
+
+  const addStaff = async () => {
+    setBusy(true);
+    setMsg("");
+    try {
+      const res = await fetch("/api/admin/staff", {
+        method: "POST",
+        headers: { "Content-Type": "application/json", "x-admin-code": adminCode },
+        body: JSON.stringify({ action: "add", name: newName, code: newCode, menus: newMenus }),
+      });
+      const d = await res.json().catch(() => ({}));
+      if (res.ok) {
+        setMsg(`✅ เพิ่ม "${newName}" แล้ว — ให้พนักงานใช้รหัสนี้ล็อกอินได้เลย`);
+        setNewName("");
+        setNewCode("");
+        load();
+      } else {
+        setMsg(`❌ ${d.error || "เพิ่มไม่สำเร็จ"}`);
+      }
+    } finally {
+      setBusy(false);
+    }
+  };
+
+  const patchStaff = async (id: string, patch: Record<string, unknown>) => {
+    await fetch("/api/admin/staff", {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json", "x-admin-code": adminCode },
+      body: JSON.stringify({ id, ...patch }),
+    });
+    load();
+  };
+
+  const removeStaff = async (s: StaffItem) => {
+    if (!confirm(`ลบบัญชีพนักงาน "${s.name}"?`)) return;
+    await fetch(`/api/admin/staff?id=${s.id}`, {
+      method: "DELETE",
+      headers: { "x-admin-code": adminCode },
+    });
+    load();
+  };
+
+  if (!loaded) {
+    return <p className="py-10 text-center text-sm text-brown-soft">กำลังโหลด…</p>;
+  }
+  if (!isOwner) {
+    return (
+      <p className="rounded-petflow bg-card p-4 text-xs font-bold text-brown-soft shadow-petflow-sm">
+        🔒 หน้านี้สำหรับเจ้าของร้านเท่านั้นค่ะ
+      </p>
+    );
+  }
+
+  const menuPicker = (selected: string[], onToggle: (href: string) => void) => (
+    <div className="grid grid-cols-2 gap-1.5">
+      {ADMIN_TABS.map((t) => (
+        <label key={t.href} className="flex items-center gap-1.5 text-[11px] text-brown">
+          <input
+            type="checkbox"
+            checked={selected.includes(t.href)}
+            onChange={() => onToggle(t.href)}
+            className="h-3.5 w-3.5 accent-[#4A7348]"
+          />
+          {t.icon} {t.label}
+        </label>
+      ))}
+    </div>
+  );
+
+  return (
+    <div className="space-y-4">
+      <section className="rounded-petflow bg-card p-4 shadow-petflow-sm">
+        <h2 className="text-sm font-extrabold text-petflow-chocolate">
+          👥 บัญชีพนักงาน ({staff.length})
+        </h2>
+        <p className="mb-3 text-[10px] text-brown-soft">
+          สร้างรหัสให้พนักงานแต่ละคน แล้วติ๊กเลือกว่าให้เห็นเมนูไหนบ้าง —
+          พนักงานล็อกอินด้วยรหัสของตัวเองที่หน้าเดิม จะเห็นเฉพาะเมนูที่เลือกไว้
+        </p>
+
+        {staff.length === 0 && (
+          <p className="mb-2 rounded-petflow-sm bg-paper/60 px-3 py-2.5 text-xs text-brown-soft">
+            ยังไม่มีพนักงาน — เพิ่มคนแรกได้ที่ช่องด้านล่างเลยค่ะ
+          </p>
+        )}
+
+        {staff.map((s) => (
+          <details
+            key={s.id}
+            className="mb-2 rounded-petflow-sm border border-petflow-line bg-paper/50 p-2.5"
+          >
+            <summary className="flex cursor-pointer list-none items-center justify-between gap-2">
+              <span className="min-w-0 truncate text-xs font-bold text-brown">
+                {s.active ? "🟢" : "⏸️"} {s.name}
+                <span className="ml-2 font-normal text-brown-faint">
+                  รหัส: {s.code} · เห็น {s.menus.length} เมนู
+                </span>
+              </span>
+              <span className="shrink-0 text-[10px] font-bold text-latte-deep">แก้ไข ▾</span>
+            </summary>
+            <div className="mt-2 space-y-2 border-t border-petflow-line pt-2">
+              {menuPicker(s.menus, (href) =>
+                patchStaff(s.id, { menus: toggleMenu(s.menus, href) })
+              )}
+              <div className="flex flex-wrap gap-2">
+                <button
+                  type="button"
+                  onClick={() => patchStaff(s.id, { active: !s.active })}
+                  className="rounded-full bg-card px-3 py-1 text-[10px] font-bold text-brown-soft"
+                >
+                  {s.active ? "⏸️ พักการใช้งาน" : "▶️ เปิดใช้งาน"}
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    const code = prompt("ตั้งรหัสใหม่ (อย่างน้อย 4 ตัว):", s.code);
+                    if (code && code.trim().length >= 4) patchStaff(s.id, { code: code.trim() });
+                  }}
+                  className="rounded-full bg-card px-3 py-1 text-[10px] font-bold text-brown-soft"
+                >
+                  🔑 เปลี่ยนรหัส
+                </button>
+                <button
+                  type="button"
+                  onClick={() => removeStaff(s)}
+                  className="rounded-full bg-wait/10 px-3 py-1 text-[10px] font-bold text-wait"
+                >
+                  🗑️ ลบ
+                </button>
+              </div>
+            </div>
+          </details>
+        ))}
+      </section>
+
+      <section className="rounded-petflow bg-card p-4 shadow-petflow-sm">
+        <h2 className="mb-2 text-sm font-extrabold text-petflow-chocolate">
+          ➕ เพิ่มพนักงานใหม่
+        </h2>
+        <div className="mb-2 grid gap-2 sm:grid-cols-2">
+          <input
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+            placeholder="ชื่อพนักงาน"
+            className="rounded-petflow-sm border border-petflow-line bg-paper px-2.5 py-2 text-xs"
+          />
+          <input
+            value={newCode}
+            onChange={(e) => setNewCode(e.target.value)}
+            placeholder="รหัสเข้าใช้ (4 ตัวขึ้นไป)"
+            className="rounded-petflow-sm border border-petflow-line bg-paper px-2.5 py-2 text-xs"
+          />
+        </div>
+        <p className="mb-1 text-[10px] font-bold text-brown-soft">เมนูที่ให้เห็น:</p>
+        {menuPicker(newMenus, (href) => setNewMenus((prev) => toggleMenu(prev, href)))}
+        <button
+          type="button"
+          disabled={busy || !newName.trim() || newCode.trim().length < 4}
+          onClick={addStaff}
+          className="mt-2 w-full rounded-petflow-sm bg-latte-deep py-2.5 text-xs font-extrabold text-card disabled:opacity-40"
+        >
+          {busy ? "กำลังเพิ่ม…" : "➕ เพิ่มพนักงาน"}
+        </button>
+        {msg && <p className="mt-2 text-[11px] font-bold text-brown">{msg}</p>}
+      </section>
+    </div>
+  );
+}
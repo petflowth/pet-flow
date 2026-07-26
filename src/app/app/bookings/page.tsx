stopstopw-bit/Catcha-hotel

Combine same-household same-time bookings into one confirm card
A family booking several cats for the same slot got a separate
"ยืนยันนัดพรุ่งนี้" push per cat — three cats meant three back-to-back
LINE messages. The daily reminder cron now groups tomorrow's bookings
by the same household+time key used for billing before building the
confirm card, joining cat names into one card with one confirm link.
Cats still needing groom history are similarly combined into one
request card (one link fills in all of them); cats that already have
history keep going to the shop's Telegram individually.

The customer's own booking-confirm page groups the same way, so
whichever cat's booking id the link happens to carry, landing on it
shows one combined card with one "ยืนยันนัด" button that confirms the
whole group via a new confirm_group action (ownership re-verified per
booking id, not just the primary). Bookings with differing status
inside a would-be group still render individually instead of silently
disappearing.

Co-Authored-By: Claude Sonnet 5 <noreply@anthropic.com>

webapp/src/app/app/bookings/page.tsx
@@ -7,8 +7,12 @@ import { t } from "@/lib/i18n";
 import { useLocale } from "@/components/LocaleProvider";
 import { useLiff } from "@/components/LiffProvider";
 import { LangSwitch } from "@/components/LangSwitch";
+import { groupBookings, groupCatNames } from "@/lib/booking-group";
 
 const CHECKIN_TIMES = ["10:00", "12:00", "14:00", "16:00", "18:00"];
+
+/** API ส่ง checkin มาด้วยจริง (ใช้จัดกลุ่มบ้านเดียวกัน/นัดเดียวกัน) แต่ type Booking กลางไม่มีฟิลด์นี้ */
+type BookingRow = Booking & { checkin?: string };
 
 function BookingsContent() {
   const { locale } = useLocale();
@@ -16,7 +20,7 @@ function BookingsContent() {
   const searchParams = useSearchParams();
   const highlightId = searchParams.get("id");
   const m = t(locale).bookings;
-  const [bookings, setBookings] = useState<Booking[]>([]);
+  const [bookings, setBookings] = useState<BookingRow[]>([]);
   const [loading, setLoading] = useState(true);
   const [pickId, setPickId] = useState<string | null>(null);
   const [checkin, setCheckin] = useState("14:00");
@@ -42,36 +46,39 @@ function BookingsContent() {
 
   useEffect(() => {
     if (!highlightId || loading || scrolledRef.current) return;
-    const target = bookings.find((b) => b.id === highlightId);
-    if (!target) return;
+    // นัดที่ถูก highlight อาจถูกรวมเป็นการ์ดเดียวกับตัวอื่นในบ้านเดียวกัน — หากลุ่มก่อน
+    // แล้วอ้างอิงด้วย id ของตัวหลักในกลุ่ม (นั่นคือ id ที่การ์ดจริงเรนเดอร์ไว้)
+    const group = groupBookings(bookings).find((g) => g.some((b) => b.id === highlightId));
+    if (!group) return;
+    const primary = group[0];
     scrolledRef.current = true;
-    if (target.status === "pending" && target.service === "room") {
-      setPickId(target.id);
+    if (primary.status === "pending" && primary.service === "room") {
+      setPickId(primary.id);
     }
     requestAnimationFrame(() => {
-      document.getElementById(`booking-${highlightId}`)?.scrollIntoView({
+      document.getElementById(`booking-${primary.id}`)?.scrollIntoView({
         behavior: "smooth",
         block: "center",
       });
     });
   }, [highlightId, loading, bookings]);
 
-  const confirm = async (id: string) => {
+  // บ้านเดียวกัน จองพร้อมกันหลายตัว → ยืนยันครั้งเดียวทั้งกลุ่ม (ids.length > 1 = ยิง
+  // confirm_group ให้ร้านได้แจ้งเตือนเป็นข้อความเดียว ไม่ใช่แยกทีละตัวเหมือนเดิม)
+  const confirmGroup = async (ids: string[], primary: BookingRow) => {
     if (confirmingId) return;
-    const b = bookings.find((x) => x.id === id);
-    if (!b) return;
-
-    setConfirmingId(id);
+    setConfirmingId(ids[0]);
     try {
       const res = await fetch("/api/bookings", {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          id,
-          action: "confirm",
+          id: ids[0],
+          ids,
+          action: ids.length > 1 ? "confirm_group" : "confirm",
           // ต้องส่ง lineUserId ไปด้วย ไม่งั้นฝั่งเซิร์ฟเวอร์เช็คว่า "นัดนี้เป็นของเราไหม" ไม่ผ่าน → 401
           lineUserId: profile?.lineUserId,
-          checkinTime: b.service === "room" ? checkin : undefined,
+          checkinTime: primary.service === "room" ? checkin : undefined,
         }),
       });
 
@@ -103,95 +110,117 @@ function BookingsContent() {
         </p>
       ) : (
         <ul className="space-y-3">
-          {bookings.map((b) => (
-            <li
-              key={b.id}
-              id={`booking-${b.id}`}
-              className={`rounded-catcha border bg-card p-4 shadow-catcha-sm ${
-                highlightId === b.id
-                  ? "border-sage ring-2 ring-sage/30"
-                  : "border-catcha-line"
-              }`}
-            >
-              <div className="mb-2 flex items-start justify-between gap-2">
-                <div>
-                  <p className="font-bold text-brown">
-                    {b.catName}{" "}
-                    <span className="font-medium text-brown-soft">· {b.customerName}</span>
-                  </p>
-                  <p className="mt-1 text-xs text-brown-soft">
-                    {b.service === "groom" ? m.groom : m.room} · {b.date}
-                    {b.time ? ` · ${b.time}` : ""}
-                  </p>
-                </div>
-                <span
-                  className={`shrink-0 rounded-full px-2.5 py-1 text-[10px] font-bold ${
-                    b.status === "confirmed"
-                      ? "bg-sage/20 text-ok"
-                      : b.status === "cancelled"
-                        ? "bg-brown-soft/15 text-brown-soft"
-                        : "bg-honey/25 text-wait"
-                  }`}
-                >
-                  {b.status === "confirmed"
-                    ? m.confirmed
-                    : b.status === "cancelled"
-                      ? m.cancelled
-                      : m.pending}
-                </span>
-              </div>
+          {groupBookings(bookings).map((group) => {
+            // บ้านเดียวกัน + นัดเดียวกัน หลายตัว → รวมเป็นการ์ดเดียว ยืนยันทีเดียวจบ
+            // (สถานะต้องตรงกันทั้งกลุ่มถึงจะรวมโชว์ — ถ้าบางตัวยืนยันไปแล้วบางตัวยังไม่
+            //  ให้แยกโชว์ทีละตัวแทน กันสับสน)
+            const sameStatus = group.every((x) => x.status === group[0].status);
+            const combine = group.length > 1 && sameStatus;
+            const primary = group[0];
+            const ids = group.map((x) => x.id);
+            const catLabel = combine ? groupCatNames(group) : primary.catName;
 
-              {b.status === "pending" && (
-                <>
-                  {b.service === "room" && pickId === b.id && (
-                    <div className="mb-3 rounded-catcha-sm bg-paper p-3">
-                      <p className="mb-2 text-xs font-bold text-brown">{m.checkin}</p>
-                      <div className="flex flex-wrap gap-2">
-                        {CHECKIN_TIMES.map((time) => (
-                          <button
-                            key={time}
-                            type="button"
-                            onClick={() => setCheckin(time)}
-                            className={`rounded-lg px-3 py-1.5 text-xs font-bold ${
-                              checkin === time
-                                ? "bg-latte-deep text-white"
-                                : "bg-card text-brown-soft"
-                            }`}
-                          >
-                            {time}
-                          </button>
-                        ))}
-                      </div>
-                    </div>
-                  )}
-                  <button
-                    type="button"
-                    disabled={confirmingId === b.id}
-                    onClick={() =>
-                      b.service === "room" && pickId !== b.id
-                        ? setPickId(b.id)
-                        : confirm(b.id)
-                    }
-                    className="flex w-full items-center justify-center gap-2 rounded-catcha-sm bg-gradient-to-r from-latte to-latte-deep py-3 text-sm font-extrabold text-white disabled:opacity-70"
+            return (
+              <li
+                key={primary.id}
+                id={`booking-${primary.id}`}
+                className={`rounded-catcha border bg-card p-4 shadow-catcha-sm ${
+                  group.some((x) => x.id === highlightId)
+                    ? "border-sage ring-2 ring-sage/30"
+                    : "border-catcha-line"
+                }`}
+              >
+                <div className="mb-2 flex items-start justify-between gap-2">
+                  <div>
+                    <p className="font-bold text-brown">
+                      {catLabel}{" "}
+                      <span className="font-medium text-brown-soft">· {primary.customerName}</span>
+                    </p>
+                    <p className="mt-1 text-xs text-brown-soft">
+                      {primary.service === "groom" ? m.groom : m.room} · {primary.date}
+                      {primary.time ? ` · ${primary.time}` : ""}
+                    </p>
+                  </div>
+                  <span
+                    className={`shrink-0 rounded-full px-2.5 py-1 text-[10px] font-bold ${
+                      primary.status === "confirmed"
+                        ? "bg-sage/20 text-ok"
+                        : primary.status === "cancelled"
+                          ? "bg-brown-soft/15 text-brown-soft"
+                          : "bg-honey/25 text-wait"
+                    }`}
                   >
-                    {confirmingId === b.id ? (
-                      <>
-                        <span className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-white/40 border-t-white" />
-                        {locale === "th" ? "กำลังยืนยัน…" : "Confirming…"}
-                      </>
-                    ) : (
-                      <>🐾 {m.confirm}</>
+                    {primary.status === "confirmed"
+                      ? m.confirmed
+                      : primary.status === "cancelled"
+                        ? m.cancelled
+                        : m.pending}
+                  </span>
+                </div>
+
+                {!combine && group.length > 1 && (
+                  <ul className="mb-2 space-y-1 text-xs text-brown-soft">
+                    {group.slice(1).map((x) => (
+                      <li key={x.id}>🐱 {x.catName}</li>
+                    ))}
+                  </ul>
+                )}
+
+                {primary.status === "pending" && (
+                  <>
+                    {primary.service === "room" && pickId === primary.id && (
+                      <div className="mb-3 rounded-catcha-sm bg-paper p-3">
+                        <p className="mb-2 text-xs font-bold text-brown">{m.checkin}</p>
+                        <div className="flex flex-wrap gap-2">
+                          {CHECKIN_TIMES.map((time) => (
+                            <button
+                              key={time}
+                              type="button"
+                              onClick={() => setCheckin(time)}
+                              className={`rounded-lg px-3 py-1.5 text-xs font-bold ${
+                                checkin === time
+                                  ? "bg-latte-deep text-white"
+                                  : "bg-card text-brown-soft"
+                              }`}
+                            >
+                              {time}
+                            </button>
+                          ))}
+                        </div>
+                      </div>
                     )}
-                  </button>
-                </>
-              )}
-              {b.checkinTime && (
-                <p className="mt-2 text-xs font-semibold text-ok">
-                  ✓ Check-in {b.checkinTime}
-                </p>
-              )}
-            </li>
-          ))}
+                    <button
+                      type="button"
+                      disabled={confirmingId === primary.id}
+                      onClick={() =>
+                        primary.service === "room" && pickId !== primary.id
+                          ? setPickId(primary.id)
+                          : confirmGroup(combine ? ids : [primary.id], primary)
+                      }
+                      className="flex w-full items-center justify-center gap-2 rounded-catcha-sm bg-gradient-to-r from-latte to-latte-deep py-3 text-sm font-extrabold text-white disabled:opacity-70"
+                    >
+                      {confirmingId === primary.id ? (
+                        <>
+                          <span className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-white/40 border-t-white" />
+                          {locale === "th" ? "กำลังยืนยัน…" : "Confirming…"}
+                        </>
+                      ) : (
+                        <>
+                          🐾 {m.confirm}
+                          {combine ? ` (${group.length} ตัว)` : ""}
+                        </>
+                      )}
+                    </button>
+                  </>
+                )}
+                {primary.checkinTime && (
+                  <p className="mt-2 text-xs font-semibold text-ok">
+                    ✓ Check-in {primary.checkinTime}
+                  </p>
+                )}
+              </li>
+            );
+          })}
         </ul>
       )}
     </div>

petflowth/pet-flow

src/app/app/bookings/page.tsx
@@ -8,8 +8,12 @@ import { useLocale } from "@/components/LocaleProvider";
 import { useLiff } from "@/components/LiffProvider";
 import { LangSwitch } from "@/components/LangSwitch";
 import { groomProgram } from "@/lib/grooming-prices";
+import { groupBookings, groupCatNames } from "@/lib/booking-group";
 
 const CHECKIN_TIMES = ["10:00", "12:00", "14:00", "16:00", "18:00"];
+
+/** API ส่ง checkin มาด้วยจริง (ใช้จัดกลุ่มบ้านเดียวกัน/นัดเดียวกัน) แต่ type Booking กลางไม่มีฟิลด์นี้ */
+type BookingRow = Booking & { checkin?: string };
 
 function BookingsContent() {
   const { locale } = useLocale();
@@ -17,7 +21,7 @@ function BookingsContent() {
   const searchParams = useSearchParams();
   const highlightId = searchParams.get("id");
   const m = t(locale).bookings;
-  const [bookings, setBookings] = useState<Booking[]>([]);
+  const [bookings, setBookings] = useState<BookingRow[]>([]);
   const [loading, setLoading] = useState(true);
   const [pickId, setPickId] = useState<string | null>(null);
   const [checkin, setCheckin] = useState("14:00");
@@ -43,36 +47,39 @@ function BookingsContent() {
 
   useEffect(() => {
     if (!highlightId || loading || scrolledRef.current) return;
-    const target = bookings.find((b) => b.id === highlightId);
-    if (!target) return;
+    // นัดที่ถูก highlight อาจถูกรวมเป็นการ์ดเดียวกับตัวอื่นในบ้านเดียวกัน — หากลุ่มก่อน
+    // แล้วอ้างอิงด้วย id ของตัวหลักในกลุ่ม (นั่นคือ id ที่การ์ดจริงเรนเดอร์ไว้)
+    const group = groupBookings(bookings).find((g) => g.some((b) => b.id === highlightId));
+    if (!group) return;
+    const primary = group[0];
     scrolledRef.current = true;
-    if (target.status === "pending" && target.service === "room") {
-      setPickId(target.id);
+    if (primary.status === "pending" && primary.service === "room") {
+      setPickId(primary.id);
     }
     requestAnimationFrame(() => {
-      document.getElementById(`booking-${highlightId}`)?.scrollIntoView({
+      document.getElementById(`booking-${primary.id}`)?.scrollIntoView({
         behavior: "smooth",
         block: "center",
       });
     });
   }, [highlightId, loading, bookings]);
 
-  const confirm = async (id: string) => {
+  // บ้านเดียวกัน จองพร้อมกันหลายตัว → ยืนยันครั้งเดียวทั้งกลุ่ม (ids.length > 1 = ยิง
+  // confirm_group ให้ร้านได้แจ้งเตือนเป็นข้อความเดียว ไม่ใช่แยกทีละตัวเหมือนเดิม)
+  const confirmGroup = async (ids: string[], primary: BookingRow) => {
     if (confirmingId) return;
-    const b = bookings.find((x) => x.id === id);
-    if (!b) return;
-
-    setConfirmingId(id);
+    setConfirmingId(ids[0]);
     try {
       const res = await fetch("/api/bookings", {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          id,
-          action: "confirm",
+          id: ids[0],
+          ids,
+          action: ids.length > 1 ? "confirm_group" : "confirm",
           // ต้องส่ง lineUserId ไปด้วย ไม่งั้นฝั่งเซิร์ฟเวอร์เช็คว่า "นัดนี้เป็นของเราไหม" ไม่ผ่าน → 401
           lineUserId: profile?.lineUserId,
-          checkinTime: b.service === "room" ? checkin : undefined,
+          checkinTime: primary.service === "room" ? checkin : undefined,
         }),
       });
 
@@ -104,110 +111,132 @@ function BookingsContent() {
         </p>
       ) : (
         <ul className="space-y-3">
-          {bookings.map((b) => (
-            <li
-              key={b.id}
-              id={`booking-${b.id}`}
-              className={`rounded-petflow border bg-card p-4 shadow-petflow-sm ${
-                highlightId === b.id
-                  ? "border-sage ring-2 ring-sage/30"
-                  : "border-petflow-line"
-              }`}
-            >
-              <div className="mb-2 flex items-start justify-between gap-2">
-                <div>
-                  <p className="font-bold text-brown">
-                    {b.catName}{" "}
-                    <span className="font-medium text-brown-soft">· {b.customerName}</span>
-                  </p>
-                  <p className="mt-1 text-xs text-brown-soft">
-                    {b.service === "groom" ? m.groom : m.room} · {b.date}
-                    {b.time ? ` · ${b.time}` : ""}
-                  </p>
-                  {b.groomProgram && (
-                    <p className="mt-0.5 text-[11px] font-bold text-latte-deep">
-                      🧴 {groomProgram(b.groomProgram)?.name || b.groomProgram}
-                    </p>
-                  )}
-                </div>
-                <span
-                  className={`shrink-0 rounded-full px-2.5 py-1 text-[10px] font-bold ${
-                    b.status === "confirmed"
-                      ? "bg-sage/20 text-ok"
-                      : b.status === "cancelled"
-                        ? "bg-brown-soft/15 text-brown-soft"
-                        : "bg-honey/25 text-wait"
-                  }`}
-                >
-                  {b.status === "confirmed"
-                    ? m.confirmed
-                    : b.status === "cancelled"
-                      ? m.cancelled
-                      : m.pending}
-                </span>
-              </div>
+          {groupBookings(bookings).map((group) => {
+            // บ้านเดียวกัน + นัดเดียวกัน หลายตัว → รวมเป็นการ์ดเดียว ยืนยันทีเดียวจบ
+            // (สถานะต้องตรงกันทั้งกลุ่มถึงจะรวมโชว์ — ถ้าบางตัวยืนยันไปแล้วบางตัวยังไม่
+            //  ให้แยกโชว์ทีละตัวแทน กันสับสน)
+            const sameStatus = group.every((x) => x.status === group[0].status);
+            const combine = group.length > 1 && sameStatus;
+            const primary = group[0];
+            const ids = group.map((x) => x.id);
+            const catLabel = combine ? groupCatNames(group) : primary.catName;
 
-              {/* นัดที่ลูกค้าจองเอง → รอร้านยืนยัน ลูกค้ายืนยันเองไม่ได้ (กัน "จองเอง ยืนยันเอง") */}
-              {b.status === "pending" && b.selfBooked && (
-                <p className="rounded-petflow-sm bg-honey/25 px-3 py-2.5 text-center text-xs font-bold text-wait">
-                  ⏳{" "}
-                  {locale === "th"
-                    ? "รอร้านยืนยันคิว — จะแจ้งกลับทาง LINE ทันทีที่ยืนยันนะคะ"
-                    : "Waiting for the shop to confirm — we'll notify you on LINE"}
-                </p>
-              )}
-              {/* นัดที่ร้านสร้างให้ → ลูกค้ากดยืนยันเข้าร่วม (ตอบรับ) ได้ตามเดิม */}
-              {b.status === "pending" && !b.selfBooked && (
-                <>
-                  {b.service === "room" && pickId === b.id && (
-                    <div className="mb-3 rounded-petflow-sm bg-paper p-3">
-                      <p className="mb-2 text-xs font-bold text-brown">{m.checkin}</p>
-                      <div className="flex flex-wrap gap-2">
-                        {CHECKIN_TIMES.map((time) => (
-                          <button
-                            key={time}
-                            type="button"
-                            onClick={() => setCheckin(time)}
-                            className={`rounded-lg px-3 py-1.5 text-xs font-bold ${
-                              checkin === time
-                                ? "bg-latte-deep text-white"
-                                : "bg-card text-brown-soft"
-                            }`}
-                          >
-                            {time}
-                          </button>
-                        ))}
-                      </div>
-                    </div>
-                  )}
-                  <button
-                    type="button"
-                    disabled={confirmingId === b.id}
-                    onClick={() =>
-                      b.service === "room" && pickId !== b.id
-                        ? setPickId(b.id)
-                        : confirm(b.id)
-                    }
-                    className="flex w-full items-center justify-center gap-2 rounded-petflow-sm bg-gradient-to-r from-latte to-latte-deep py-3 text-sm font-extrabold text-white disabled:opacity-70"
-                  >
-                    {confirmingId === b.id ? (
-                      <>
-                        <span className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-white/40 border-t-white" />
-                        {locale === "th" ? "กำลังยืนยัน…" : "Confirming…"}
-                      </>
-                    ) : (
-                      <>🐾 {m.confirm}</>
+            return (
+              <li
+                key={primary.id}
+                id={`booking-${primary.id}`}
+                className={`rounded-petflow border bg-card p-4 shadow-petflow-sm ${
+                  group.some((x) => x.id === highlightId)
+                    ? "border-sage ring-2 ring-sage/30"
+                    : "border-petflow-line"
+                }`}
+              >
+                <div className="mb-2 flex items-start justify-between gap-2">
+                  <div>
+                    <p className="font-bold text-brown">
+                      {catLabel}{" "}
+                      <span className="font-medium text-brown-soft">· {primary.customerName}</span>
+                    </p>
+                    <p className="mt-1 text-xs text-brown-soft">
+                      {primary.service === "groom" ? m.groom : m.room} · {primary.date}
+                      {primary.time ? ` · ${primary.time}` : ""}
+                    </p>
+                    {primary.groomProgram && (
+                      <p className="mt-0.5 text-[11px] font-bold text-latte-deep">
+                        🧴 {groomProgram(primary.groomProgram)?.name || primary.groomProgram}
+                      </p>
                     )}
-                  </button>
-                </>
-              )}
-              {b.checkinTime && (
-                <p className="mt-2 text-xs font-semibold text-ok">
-                  ✓ Check-in {b.checkinTime}
-                </p>
-              )}
-            </li>
-          ))}
+                  </div>
+                  <span
+                    className={`shrink-0 rounded-full px-2.5 py-1 text-[10px] font-bold ${
+                      primary.status === "confirmed"
+                        ? "bg-sage/20 text-ok"
+                        : primary.status === "cancelled"
+                          ? "bg-brown-soft/15 text-brown-soft"
+                          : "bg-honey/25 text-wait"
+                    }`}
+                  >
+                    {primary.status === "confirmed"
+                      ? m.confirmed
+                      : primary.status === "cancelled"
+                        ? m.cancelled
+                        : m.pending}
+                  </span>
+                </div>
+
+                {!combine && group.length > 1 && (
+                  <ul className="mb-2 space-y-1 text-xs text-brown-soft">
+                    {group.slice(1).map((x) => (
+                      <li key={x.id}>🐱 {x.catName}</li>
+                    ))}
+                  </ul>
+                )}
+
+                {/* นัดที่ลูกค้าจองเอง → รอร้านยืนยัน ลูกค้ายืนยันเองไม่ได้ (กัน "จองเอง ยืนยันเอง") */}
+                {primary.status === "pending" && primary.selfBooked && (
+                  <p className="rounded-petflow-sm bg-honey/25 px-3 py-2.5 text-center text-xs font-bold text-wait">
+                    ⏳{" "}
+                    {locale === "th"
+                      ? "รอร้านยืนยันคิว — จะแจ้งกลับทาง LINE ทันทีที่ยืนยันนะคะ"
+                      : "Waiting for the shop to confirm — we'll notify you on LINE"}
+                  </p>
+                )}
+                {/* นัดที่ร้านสร้างให้ → ลูกค้ากดยืนยันเข้าร่วม (ตอบรับ) ได้ตามเดิม */}
+                {primary.status === "pending" && !primary.selfBooked && (
+                  <>
+                    {primary.service === "room" && pickId === primary.id && (
+                      <div className="mb-3 rounded-petflow-sm bg-paper p-3">
+                        <p className="mb-2 text-xs font-bold text-brown">{m.checkin}</p>
+                        <div className="flex flex-wrap gap-2">
+                          {CHECKIN_TIMES.map((time) => (
+                            <button
+                              key={time}
+                              type="button"
+                              onClick={() => setCheckin(time)}
+                              className={`rounded-lg px-3 py-1.5 text-xs font-bold ${
+                                checkin === time
+                                  ? "bg-latte-deep text-white"
+                                  : "bg-card text-brown-soft"
+                              }`}
+                            >
+                              {time}
+                            </button>
+                          ))}
+                        </div>
+                      </div>
+                    )}
+                    <button
+                      type="button"
+                      disabled={confirmingId === primary.id}
+                      onClick={() =>
+                        primary.service === "room" && pickId !== primary.id
+                          ? setPickId(primary.id)
+                          : confirmGroup(combine ? ids : [primary.id], primary)
+                      }
+                      className="flex w-full items-center justify-center gap-2 rounded-petflow-sm bg-gradient-to-r from-latte to-latte-deep py-3 text-sm font-extrabold text-white disabled:opacity-70"
+                    >
+                      {confirmingId === primary.id ? (
+                        <>
+                          <span className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-white/40 border-t-white" />
+                          {locale === "th" ? "กำลังยืนยัน…" : "Confirming…"}
+                        </>
+                      ) : (
+                        <>
+                          🐾 {m.confirm}
+                          {combine ? ` (${group.length} ตัว)` : ""}
+                        </>
+                      )}
+                    </button>
+                  </>
+                )}
+                {primary.checkinTime && (
+                  <p className="mt-2 text-xs font-semibold text-ok">
+                    ✓ Check-in {primary.checkinTime}
+                  </p>
+                )}
+              </li>
+            );
+          })}
         </ul>
       )}
     </div>